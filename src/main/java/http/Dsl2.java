41f65c364a85b3d8d1a33dc6a506f272e7c56493
over <overln2@gmail.com>
2026-03-31 19:00:15 +0600

    in progress

--- src/main/java/http/Dsl2.java
+++ src/main/java/http/Dsl2.java
@@ -2,26 +2,77 @@ package http;
 
 import java.util.UUID;
 
+// Dsl2.hello
 public class Dsl2 {
-    @interface StatusCode {}
-    @interface MimeType {}
-    @interface Header {}
-    @interface QueryParameter {}
+    // Content-Encoding
+    // Загрузка и выгрузка файлов
 
-    @MimeType @interface XWwwFormUrlencoded {}
-    @MimeType @interface ApplicationJson {}
-    @MimeType @interface TextPlain {}
+    @interface HttpMethod {
+        String value();
+    }
 
-    @StatusCode @interface Sc200 {}
-    @StatusCode @interface Sc400 {}
-    @StatusCode @interface Sc500 {}
-    @StatusCode @interface Sc511 {}
+    @HttpMethod("GET") @interface Get {
+        String value() default "__magic__true-service_not_defined";
+    }
+    @HttpMethod("POST") @interface Post {
+        String value() default "__magic__true-service_not_defined";
+    }
+    @HttpMethod("DELETE") @interface Delete {
+        String value() default "__magic__true-service_not_defined";
+    }
+    @HttpMethod("X-My") @interface XMy {
+        String value() default "__magic__true-service_not_defined";
+    }
 
-    record Authorization() {}
+    @interface UriPathRoute {}
+    @interface UriPathParameters {}
+    @interface UriQuery {}
+    @interface PathParameter { }
+    @interface QueryParameter { }
+
+    @interface MimeType {
+        String type();
+        String subtype();
+    }
+
+    @interface Header { }
+
+    @MimeType(type = "multipart", subtype = "form-data") @interface MultipartFormData { }
+    @MimeType(type = "application", subtype = "json") @interface ApplicationJson { }
+    @MimeType(type = "text", subtype = "plain") @interface TextPlain { }
+
+    @interface StatusCode {
+        int code();
+        String reasonPhrase();
+    }
+
+    @StatusCode(code = 200, reasonPhrase = "OK") @interface Sc200 { }
+    @StatusCode(code = 400, reasonPhrase = "Bad request") @interface Sc400 { }
+    @StatusCode(code = 500, reasonPhrase = "Internal server error") @interface Sc500 { }
+    @StatusCode(code = 511, reasonPhrase = "Application logic error") @interface Sc511 { }
+
+
+    // Headers
+    record Authorization() { }
+    record SetCookie<P>() {
+        SetCookie<P> setDomain() { return this; }
+        SetCookie<P> setHttpOnly() { return this; }
+    }
+
+    @interface CookiePart {
+        String value(); // name
+    }
+
+    @CookiePart("JSESSIONID") record JSessionId(long id) {}
 
     String endpoint1(int a, int b) {
         return "";
     }
+
+    // route, method
+    // @Get, @Post,
+    // @Route("/")
+
 
     // Union2<
     //     @Sc200 @TextPlain String,
@@ -31,15 +82,31 @@ public class Dsl2 {
     //     return Union2.of1("hello");
     // }
 
-    @Sc400 @TextPlain String hello() {
+    // @Route("XXX", path="/")
+    // new SetCookie(new JSessionId(12312))
+    //     .setDomain(...)
+    //     .setHttpOnly(...)
+
+    @Get("/") @Sc400 @TextPlain String hello() {
         return "hello";
     }
 
+    // new TrueService()
+    //     .with(new CorsMiddleware(), H
+    //         .http(new MyApi())
+    //     )
+    //
+
+    // 1. GET /
+    //    OPTIONS / => CORS POLICY (HEADERS)
+    // 2. GET /
+
     @Sc400 @TextPlain String endpoint1(
-        @QueryParameter     UUID          id,
-        @Header             Authorization authorization,
-        @XWwwFormUrlencoded int           a,
-        @XWwwFormUrlencoded int           b
+        @QueryParameter UUID id,
+        @Header Authorization authorization,
+        @MultipartFormData int a,
+        @MultipartFormData int b,
+        @Header JSessionId sessionId
     ) {
         return "";
     }
